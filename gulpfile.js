15fdee813b0d468043e93d6e11780cdacd91baea
ilocation <ilocation@yeah.net>
2016-05-01 09:58:05 +0800

    add debug version p-ol3.debug.js; update gulp-uglify version; use p-ol3 debug version in sample.

--- gulpfile.js
+++ gulpfile.js
@@ -67,6 +67,44 @@ gulp.task('compact-js', function () {
        .pipe(gulp.dest('./sample/'));
 });
 
+gulp.task('compact-js-debug', function () {
+    return gulp.src(['./src/gispace/GISpace.js',
+            './src/gispace/Constants.js',
+            './src/gispace/util/Utils.js',
+            './src/gispace/util/DomUtils.js',
+            './src/gispace/PlotTypes.js',
+            './src/gispace/PlotUtils.js',
+            './src/gispace/event/Event.js',
+            './src/gispace/event/PlotDrawEvent.js',
+            './src/gispace/plot/Plot.js',
+            './src/gispace/plot/Arc.js',
+            './src/gispace/plot/AttackArrow.js',
+            './src/gispace/plot/SquadCombat.js',
+            './src/gispace/plot/TailedAttackArrow.js',
+            './src/gispace/plot/TailedSquadCombat.js',
+            './src/gispace/plot/Circle.js',
+            './src/gispace/plot/ClosedCurve.js',
+            './src/gispace/plot/Curve.js',
+            './src/gispace/plot/DoubleArrow.js',
+            './src/gispace/plot/Ellipse.js',
+            './src/gispace/plot/FineArrow.js',
+            './src/gispace/plot/AssaultDirection.js',
+            './src/gispace/plot/GatheringPlace.js',
+            './src/gispace/plot/Lune.js',
+            './src/gispace/plot/Sector.js',
+            './src/gispace/plot/StraightArrow.js',
+            './src/gispace/plot/Polyline.js',
+            './src/gispace/plot/FreehandLine.js',
+            './src/gispace/plot/Polygon.js',
+            './src/gispace/plot/FreehandPolygon.js',
+            './src/gispace/PlotFactory.js',
+            './src/gispace/tool/PlotDraw.js',
+            './src/gispace/tool/PlotEdit.js'])
+        .pipe(concat('p-ol3.debug.js'))
+        .pipe(gulp.dest('./build/'))
+        .pipe(gulp.dest('./sample/'));
+});
+
 gulp.task('compact-css', function(){
     return gulp.src('src/*.css')
         .pipe(concat('p-ol3.min.css'))
@@ -76,7 +114,7 @@ gulp.task('compact-css', function(){
 });
 
 gulp.task('default', function () {
-    var jsWatch = gulp.watch('./src/**/*.js', ['compact-js']);
+    var jsWatch = gulp.watch('./src/**/*.js', ['compact-js', 'compact-js-debug']);
     jsWatch.on('change', function (e) {
         console.log('File ' + e.path + ' was ' + e.type + ', running compact js ...');
     });
